a36fb468f8b7d798328591d0fed8eb5f105efceb
Keira <keira.liddle22@gmail.com>
2026-08-25 16:47:08 +1000

    Router change

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
 import Home from '../pages/Home';
 import Projects from '../pages/Project';
 
